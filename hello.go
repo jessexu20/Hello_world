b4a291ec0887354f5d9a9fc2864d3a8e4b39e9d7
Jesse <jesse.xu.20@gmail.com>
2015-05-20 15:08:13 -0400

    import stringutil

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+	"github.com/jessexu20/stringutil"
+)
 func main() {
+	fmt.Printf(stringutil.Reverse("abcdefg"))
 	fmt.Printf("Hello, world.\n")
 }
